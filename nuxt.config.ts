5fbb8ac4a0912fe8a5a6599fd93252fb6f61f494
JRosSx91 <hazee.1491@gmail.com>
2024-03-15 16:31:03 +0100

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'AWay - Tu app del camino',
+    },
+  },
   css: ['~/assets/css/globals.css'],
   postcss: {
     plugins: {
---
+++ pages/login.vue
@@ -0,0 +1,2 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template><h1>TODO</h1></template>
